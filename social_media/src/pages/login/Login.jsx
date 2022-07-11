ed16ebf8788f4cd9465ec7399acad5bef2a056b8
Zohaib <zohaibmanzoor1700@gmail.com>
2022-07-11 16:10:26 -0700

    reworkedSignupAndLoginPage

--- social_media/src/pages/login/Login.jsx
+++ social_media/src/pages/login/Login.jsx
@@ -1,3 +1,16 @@
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Checkbox from '@mui/material/Checkbox';
+import Container from '@mui/material/Container';
+import CssBaseline from '@mui/material/CssBaseline';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Grid from '@mui/material/Grid';
+import Link from '@mui/material/Link';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
 import { useContext, useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import AppContext from "../../context/appContext";
@@ -42,61 +55,90 @@ export default function Login({ setAuth }) {
     setPassword("");
   };
 
+  function Copyright(props) {
+    return (
+      <Typography variant="body2" color="text.secondary" align="center" {...props}>
+        {'Copyright © '}
+        <Link color="inherit" href="https://mui.com/">
+          Trip by Trip
+        </Link>{' '}
+        {new Date().getFullYear()}
+        {'.'}
+      </Typography>
+    );
+  }
+  
+  const theme = createTheme();
   return (
-    <div className="page">
-      <div className="left-side"></div>
-      <div className="right-side">
-        <span className="logo">Trip by Trip</span>
-        <div>
-          <span className="tagline">Food justice community</span>
-        </div>
-        <div className="pageSwitcherL">
-          <NavLink
-            to="/login"
-            activeClassName="pageSwitcherItem-activeL"
-            className="pageSwitcherItemL"
-          >
-            Sign In
-          </NavLink>
-          <NavLink
-            exact
-            to="/signup"
-            activeClassName="pageSwitcherItem-activeL"
-            className="pageSwitcherItemL"
-          >
-            Sign Up
-          </NavLink>
-        </div>
-        <div className="form-outline">
-          <input
-            type="email"
-            id="form3Example1w"
-            className="form-control"
+    <ThemeProvider theme={theme}>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }} className="avatarPic">
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <Box component="form" onSubmit={handleLogin} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
             onChange={(e) => setEmail(e.target.value)}
           />
-          <label className="formLabel" htmlFor="form3Example1w ">
-            Email
-          </label>
-        </div>
-
-        <div className="form-outline">
-          <input
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
             type="password"
-            id="form3Example1w"
-            className="form-control"
+            id="password"
+            autoComplete="current-password"
             onChange={(e) => setPassword(e.target.value)}
           />
-          <label className="formLabel" htmlFor="form3Example1w">
-            Password
-          </label>
-        </div>
-
-        <div className="form-outline">
-          <button type="submit" className="button" onClick={handleLogin}>
-            Submit
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+            onClick={handleLogin}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item className="toSignUp">
+              <NavLink to="/signup">
+              {"Don't have an account? Sign Up"}
+              </NavLink>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+      <Copyright sx={{ mt: 8, mb: 4 }} className="copyright"/>
+    </Container>
+  </ThemeProvider>
+  )
 }
--- social_media/src/pages/signup/Signup.jsx
+++ social_media/src/pages/signup/Signup.jsx
@@ -1,7 +1,18 @@
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Avatar from "@mui/material/Avatar";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import CssBaseline from "@mui/material/CssBaseline";
+import Grid from "@mui/material/Grid";
+import Link from "@mui/material/Link";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
 import { useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
-// import "./signup.css";
 import "../login/login.css";
+import "./signup.css";
 
 export default function Signup() {
   const navigate = useNavigate();
@@ -30,74 +41,110 @@ export default function Signup() {
     navigate("/login", { replace: true });
   };
 
+  function Copyright(props) {
+    return (
+      <Typography
+        variant="body2"
+        color="text.secondary"
+        align="center"
+        {...props}
+      >
+        {"Copyright © "}
+        <Link color="inherit" href="https://mui.com/">
+          Trip by Trip
+        </Link>{" "}
+        {new Date().getFullYear()}
+        {"."}
+      </Typography>
+    );
+  }
+
+  const theme = createTheme();
+
   return (
-    <div className="page">
-      <div className="left-side">
-      </div>
-      <div className="right-side">
-        <span className="logo">Grateful Gardens</span>
-        <div>
-          <span className="tagline">Food justice community</span>
-        </div>
-        <div className="pageSwitcherS">
-          <NavLink
-            to="/login"
-            activeClassName="pageSwitcherItem-activeS"
-            className="pageSwitcherItemS"
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar
+            sx={{ m: 1, bgcolor: "secondary.main" }}
+            className="avatarPic"
           >
-            Sign In
-          </NavLink>
-          <NavLink
-            exact
-            to="/signup"
-            activeClassName="pageSwitcherItem-activeS"
-            className="pageSwitcherItemS"
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleSignup}
+            sx={{ mt: 3 }}
           >
-            Sign Up
-          </NavLink>
-        </div>
-        <div className="form-outline">
-          <input
-            type="email"
-            id="form3Example1w"
-            className="form-control"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <label className="formLabel" htmlFor="form3Example1w ">
-            Email
-          </label>
-        </div>
+            <Grid container spacing={2}>
+              <Grid item xs={12} onChange={(e) => setUsername(e.target.value)}>
+                <TextField
+                  autoComplete="given-name"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="User Name"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} onChange={(e) => setEmail(e.target.value)}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12} onChange={(e) => setPassword(e.target.value)}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+            </Grid>
+            <NavLink to="/login">
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+                onClick={handleSignup}
+              >
+                Sign Up
+              </Button>
+            </NavLink>
 
-        <div className="form-outline">
-          <input
-            type="password"
-            id="form3Example1w"
-            className="form-control"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <label className="formLabel" htmlFor="form3Example1w">
-            Password
-          </label>
-        </div>
-
-        <div className="form-outline">
-          <input
-            type="text"
-            id="form3Example1w"
-            className="form-control"
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <label className="formLabel" htmlFor="form3Example1w">
-            Username
-          </label>
-        </div>
-
-        <div className="form-outline">
-          <button type="submit" className="button" onClick={handleSignup}>
-            Submit
-          </button>
-        </div>
-      </div>
-    </div>
+            <Grid container className="signIn">
+              <Grid item>
+                <NavLink to="/login" variant="body2">
+                  Already have an account? Sign in
+                </NavLink>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </ThemeProvider>
   );
 }
