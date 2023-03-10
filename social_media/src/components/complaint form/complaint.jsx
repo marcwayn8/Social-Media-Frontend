4321ff9cd61ef4049c33cbeccb5ed13030083fe8
Wayne March <93327533+marcwayn8@users.noreply.github.com>
2023-03-09 20:41:56 -0500

    code refactored

--- social_media/src/components/complaint form/complaint.jsx
+++ social_media/src/components/complaint form/complaint.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect ,Fragment} from 'react';
 import axios from 'axios';
 import L from 'leaflet';
 import { useContext } from 'react';
